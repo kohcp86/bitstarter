454f1908cfb358c0c6d122e225d7d2d86ef9f8e1
stoneK <macca8_koh@hotmail.com>
2013-07-06 16:39:26 +0000

    Altered web.js

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   //var fs = require('fs');
   //var buffer = new buffer();
-  //response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))});
+  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))});
   response.send('Test');
 });
 
