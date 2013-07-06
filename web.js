994ad5d67fe2437e29ae44cc65952a112df86cb2
stoneK <macca8_koh@hotmail.com>
2013-07-06 16:57:08 +0000

    Altered web.js

--- web.js
+++ web.js
@@ -7,7 +7,8 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   //var fs = require('fs');
   var buf = new Buffer(256);
-  buf.write(fs.readFileSync("index.html"));
+  //buf.write(fs.readFileSync('index.html'));
+	buf = fs.readFileSync('index.html);
   response.send(buf.toString());
   //response.send(buf.toString('utf8', fs.readFileSync("index.html")));//});
   //response.send('Test');
