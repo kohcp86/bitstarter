b1946933540bc92d2fa19ddd5fcd8430d8d9794f
stoneK <macca8_koh@hotmail.com>
2013-07-06 17:00:59 +0000

    Altered web.js

--- web.js
+++ web.js
@@ -1,17 +1,13 @@
 var express = require('express');
 var fs = require('fs');
-var buffer = require('buffer');
+//var buffer = require('buffer');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  //var fs = require('fs');
   var buf = new Buffer(256);
-  //buf.write(fs.readFileSync('index.html'));
-	buf = fs.readFileSync('index.html');
+  buf = fs.readFileSync('index.html');
   response.send(buf.toString());
-  //response.send(buf.toString('utf8', fs.readFileSync("index.html")));//});
-  //response.send('Test');
 });
 
 var port = process.env.PORT || 5000;
