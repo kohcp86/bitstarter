e3c78c09c88e100cfa49b5c507658d445ceb23a7
stoneK <macca8_koh@hotmail.com>
2013-07-07 08:24:19 +0000

    Altered grader.js and checks.json

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://cryptic-brook-3636.herokuapp.com";
+var TEMPFILE = "tmp";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -63,12 +66,21 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json'/*, clone(assertFileExists)*/, CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html'/*, clone(assertFileExists)*/, HTMLFILE_DEFAULT)
+		.option('-u, --url <url>', 'Url to index.html'/*, clone(assertFileExists)*/, URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	rest.get(program.url).on('complete', function(result) {
+		fs.writeFile(TEMPFILE, result, function(err) {
+			if(err) {
+				console.log(err);
+			} else {
+				var checkJson = checkHtmlFile(TEMPFILE, program.checks);
+				var outJson = JSON.stringify(checkJson, null, 4);
+				console.log(outJson);
+			}
+		}); 
+	});
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
